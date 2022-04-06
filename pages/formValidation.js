02a7618cd4c13eb380c02b016e60e5fae475e75e
Tanya Osipova <t0sipova5@yandex.ru>
2022-04-06 14:43:26 +0200

    fix: correct nav padding

--- pages/formValidation.js
+++ pages/formValidation.js
@@ -72,32 +72,30 @@ const enableValidation = () => {
 enableValidation(); 
 
 // Navigation
-const navMenu = document.querySelector('.nav__burger-menu');
-const navList = document.querySelector('.nav__list');
+const navMenuButton = document.querySelector('.nav__burger-menu');
 const closeButton = document.querySelector('.nav__close-button');
+const navList = document.querySelector('.nav__list');
+const navLinkList = navList.querySelectorAll(".nav__link");
+
 
 function openMenu() {
   navList.classList.add('nav__list_show');
-  navMenu.classList.add('nav__burger-menu_hide');
+  navMenuButton.classList.add('nav__burger-menu_hide');
   closeButton.classList.add('nav__close-button_show')
   
 }
 
 function closeMenu() {
   navList.classList.remove('nav__list_show');
-  navMenu.classList.remove('nav__burger-menu_hide');
+  navMenuButton.classList.remove('nav__burger-menu_hide');
   closeButton.classList.remove('nav__close-button_show')
 }
 
-navMenu.addEventListener('click', openMenu)
+navMenuButton.addEventListener('click', openMenu)
 closeButton.addEventListener('click', closeMenu)
 
 
 // Add active class to the current button 
-
-const navContainer = document.querySelector(".nav__list");
-const navLinkList = navContainer.querySelectorAll(".nav__link");
-
 for (let i = 0; i < navLinkList.length; i++) {
   navLinkList[i].addEventListener("click", function() {
   const currentLink = document.querySelectorAll(".nav__link_type_active");
