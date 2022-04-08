c98804c4dd6e03e18213a4394d31f20452be75cb
Tanya Osipova <t0sipova5@yandex.ru>
2022-04-09 00:42:43 +0200

    feat: add about

---
+++ pages/swiperAbout.js
@@ -0,0 +1,50 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 2,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  // Navigation
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Grab cursor
+  grabCursor: true,
+
+  // Keyboard control
+  keyboard: {
+    enabled: true,
+  },
+
+  // Mousewheel control
+  mousewheel: true,
+
+  // Lazy load images
+  lazy: true,
+
+  // Height
+  autoHeight: true,
+  //Responsive breakpoints
+  // breakpoints: {
+  //     640: {
+  //           slidesPerView: 1,
+            
+  //         },
+  //         768: {
+  //           slidesPerView: 2,
+            
+  //         },
+  //         1024: {
+  //           slidesPerView: 3,
+  //         },
+  //       }
+});
