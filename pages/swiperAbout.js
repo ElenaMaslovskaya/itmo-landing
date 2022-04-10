0316c8851020240631ef52e894e506030cb22cd1
Tanya Osipova <t0sipova5@yandex.ru>
2022-04-10 20:18:01 +0200

    feat: add swiper laptop version

--- pages/swiperAbout.js
+++ pages/swiperAbout.js
@@ -1,94 +1,41 @@
-
-
-
-
-/*
-const swiper = new Swiper('.swiper', {
-
+const swiper = new Swiper(".swiper", {
   autoHeight: true,
-  slidesPerView: 3,
   
-  slidesPerGroup: 3,
-  loop: true,
-  
-
-  
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  
-  
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
- 
+  grabCursor:true,
+  mousewheel: true,
   grabCursor: true,
-
-
   keyboard: {
     enabled: true,
   },
-
-  
-  mousewheel: true,
-
-  
-  lazy: true,
-
-  
-
+       
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   breakpoints: {
     300: {
       slidesPerView: 'auto',
-      slidesPerGroup: 0,
+      loop: true,
       centeredSlides: true,
-      spaceBetween: 10,
     },
-    600: {
+    700: {
       slidesPerView: 2.2,
-      slidesPerGroup: 0,
-      spaceBetween: 30,
+      slidesPerGroup: 2,
+      spaceBetween: 16,
+      loop: false,
+      centeredSlides: false,
     },
     1100: {
       slidesPerView: 3,
       slidesPerGroup: 3,
-      spaceBetween: 32,
+      spaceBetween: 20,
+      loop: true,
+      centeredSlides: true,
     },
-  }
+  },
 });
-*/
-const swiper = new Swiper(".swiper", {
-  autoHeight: true,
-  
-  loop: true,
-  centeredSlides: true,
-  grabCursor:true,
-       
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          300: {
-            slidesPerView: 'auto',
-          },
-          600: {
-            slidesPerView: 2,
-            spaceBetween: 16,
-          
-          },
-          1100: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-            slidesPerGroup: 3,
-          },
-        },
-      });
  
