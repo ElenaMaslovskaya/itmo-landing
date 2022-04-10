fe04d122027d1c53c82fb63b5e9f79ff176f3189
Tanya Osipova <t0sipova5@yandex.ru>
2022-04-10 05:24:46 +0200

    feat: add swiper tablet version

--- pages/swiperAbout.js
+++ pages/swiperAbout.js
@@ -1,10 +1,11 @@
 const swiper = new Swiper('.swiper', {
-  
+  // Height
+  autoHeight: true,
   slidesPerView: 3,
   //spaceBetween: 2,
   slidesPerGroup: 3,
   loop: true,
-  loopFillGroupWithBlank: true,
+  //loopFillGroupWithBlank: true,
 
   // Navigation
   navigation: {
@@ -22,16 +23,36 @@ const swiper = new Swiper('.swiper', {
   grabCursor: true,
 
   // Keyboard control
-  keyboard: {
-    enabled: true,
-  },
+  //keyboard: {
+    //enabled: true,
+  //},
 
   // Mousewheel control
-  mousewheel: true,
+  //mousewheel: true,
 
   // Lazy load images
-  lazy: true,
+  //lazy: true,
 
-  // Height
-  autoHeight: true,
+  
+
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      slidesPerGroup: 0,
+      centeredSlides: true,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 2.2,
+      slidesPerGroup: 0,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 32,
+    },
+  }
 });
+
+ 
